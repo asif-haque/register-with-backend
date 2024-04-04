92c60c2a2e36cf3ae1c439e0042a172e027560ae
Asif <asifhaque0505@gmail.com>
2024-04-04 21:57:26 +0530

    input name added

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-var cors = require("cors"); 
+var cors = require("cors");
 
 const app = express();
 const port = 3000;
-
 
 mongoose
   .connect("mongodb://localhost:27017/UserDB")
@@ -15,18 +14,18 @@ mongoose
     console.error("Error connecting to MongoDB:", err);
   });
 
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
-}, { collection: 'users' });
+const userSchema = new mongoose.Schema(
+  {
+    username: String,
+    email: String,
+    password: String,
+  },
+  { collection: "users" }
+);
 const User = mongoose.model("User", userSchema);
-
 
 app.use(express.json());
 app.use(cors());
-
 
 app.post("/register", (req, res) => {
   const userData = req.body;
@@ -41,20 +40,20 @@ app.post("/register", (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
+  console.log(req.body);
   try {
     const user = await User.findOne({ username: username });
+    console.log(user);
     if (!user || user.password !== password) {
       return res.status(401).json({ message: "Invalid credentials" });
-    }
-    else{
-    return res.status(200).json({ message: "Login successful" });
+    } else {
+      return res.status(200).json({ message: "Login successful" });
     }
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: "Internal server error" });
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
